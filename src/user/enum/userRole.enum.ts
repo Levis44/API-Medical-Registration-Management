0f3234826f4a195d9b2b14ad1d5b9b3cabff1bee
Levi <levi.ciarrocchi@gmail.com>
2021-11-09 15:00:23 -0300

    User entity

---
+++ src/user/enum/userRole.enum.ts
@@ -0,0 +1,4 @@
+export enum UserStatus {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserStatus } from './enum/userRole.enum';
+
+@Entity()
+export class Specialty {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'enum', enum: UserStatus, default: UserStatus.USER })
+  role: UserStatus;
+}
